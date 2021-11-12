29683ae3ee2461362e78b45308ace7f147e3f947
WintonDeVilliers <wintondvllrs2@gmail.com>
2021-11-12 12:01:23 +0200

    added yaml constructs for each component, can make layer 2 dirs hardcoded, resolve with variable *args

--- DirectoryActions.py
+++ DirectoryActions.py
@@ -8,19 +8,41 @@ import yamldirs.yamldirs_cmd
 
 from FileLoader import *
 
-# main Path env variable
+# Path variables/ env variables
 load_dotenv()
 main_root = os.getenv("MAIN_ROOT")
 top_layer_dir = os.getenv("TOP_LAYER_DIR")
 addons = os.getenv("ADDONS")
+# continue env vars
+mk_construct = os.getenv("MK")
+software_construct = os.getenv("SOFTWARE")
+"""if in folder"""
+# temp_construct = no path set
+pre_production_construct = os.getenv("PRE_PRODUCTION")
+# audio_construct = no path set
+_2d_construct = os.getenv("_2D")
+_3d_construct = os.getenv("_3D")
+# references_construct = no path set
+shoot_data_construct = os.getenv("SHOOT_DATA")
+""" if out folder"""
+_approvals_construct = os.getenv("_APPROVALS")
+_data_exchange_construct = os.getenv("_DATA_EXCHANGE")
+_finals_construct = os.getenv("_FINALS")
+""" if work folder"""
+_global_construct = os.getenv("_GLOBAL")
+assets_construct = os.getenv("ASSETS")
+TVC_or_FLM_construct = os.getenv("TVC_OR_FLM")
+# shots_construct = path not set
+
+
 
 """LAYER 1
 will activate first two blocks """
 
 
 # will gen the default top-level directory in our main_root folder
-def make_default_dirs(default=f"Default{datetime.timestamp(datetime.now())}"):
-    project_name_leaf = input(f"Project Name or press ENTER to use {default}")  # this must be a radar selection
+def make_default_dirs(project_name_leaf, default=f"Default{datetime.timestamp(datetime.now())}"):
+    # project_name_leaf = input(f"Project Name or press ENTER to use {default}")  # this must be a radar selection
     input_check = re.search(r'([a-zA-Z]|[0-9])', project_name_leaf)
     if input_check is None:
         project_name_leaf = default
@@ -64,12 +86,13 @@ def add_to_given_directory():
 
 
 """Layer2
-will activate blocks of dirs"""
+will activate blocks of dirs
+we will try positional args, with the view changing it at next refactor to accommodate **kwargs"""
 
 
-def edit_existing_directory():
+def edit_existing_directory(user_search_project, *args):
     os.chdir(main_root)
-    user_search_project = input("What project do you want to edit: ")
+    # user_search_project = input("What project do you want to edit: ")
     selected_proj_path = main_root + os.sep + user_search_project
     os.chdir(selected_proj_path)
     # at this point we are in the dir and have sight of the base folders
@@ -77,14 +100,14 @@ def edit_existing_directory():
         # if path in sub_dirs:
         for i in sub_dirs:
             if i.endswith("mk"):
-                os.chdir(path + os.sep + 'mk')
+                os.chdir(path + os.sep + "mk")
                 print(os.getcwd())
-                yamldirs.yamldirs_cmd.reconstitute_directory("C:\\Users\\CHOCO-012\\PycharmProjects\\vfx_dir_tool\\mk_construct.yaml")
+                yamldirs.yamldirs_cmd.reconstitute_directory(mk_construct)
 
             # main_root + os.sep + user_search_project + os.sep + folder_to_edit
 
 
-edit_existing_directory()
+# edit_existing_directory()
 # ------------------------------------------ Function calls --------------------------------------------------------
 # make_default_dirs()
 
---
+++ MyPaths.py
@@ -0,0 +1,39 @@
+""" Here we must try house the paths in a class, such that they can be called in our DirectoryActions.py without having
+to load all paths for each function call"""
+
+# import os
+#
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# main_root = os.getenv("MAIN_ROOT")
+# top_layer_dir = os.getenv("TOP_LAYER_DIR")
+# addons = os.getenv("ADDONS")
+# # continue env vars
+# mk_construct = os.getenv()
+# software_construct = os.getenv()
+# """in folder"""
+# # temp_construct = no path set
+# pre_production_construct = os.getenv()
+# audio_construct = os.getenv()
+# _2d_construct = os.getenv()
+# _3d_construct = os.getenv()
+# references = os.getenv()
+# shoot_data = os.getenv()
+# """out folder"""
+# _approvals = os.getenv()
+# _data_exchange = os.getenv()
+# _finals = os.getenv()
+
+
+"""class MyPaths:
+    # def __init__(self, path_request):
+    #     self.path_request = path_request
+    # 
+    # 
+    # def use_path(self, param):
+    #     
+    #     if param == "addons":
+    #         return self
+        
+        pass"""
